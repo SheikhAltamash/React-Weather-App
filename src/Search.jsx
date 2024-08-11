fb37fe0bcfc37a51020e91998300501f211f4aea
altamash <altamashsheikh077@gmail.com>
2024-08-11 19:43:40 +0530

    added new functionality

---
+++ src/Search.jsx
@@ -0,0 +1,77 @@
+import SearchIcon from "@mui/icons-material/Search";
+import Button from "@mui/material/Button";
+import {convertUnixTimestampTo12HourFormat}  from "./helperfunctions.js";
+import "./search.css";
+import { useEffect, useState } from "react";
+export default function SearchBox({updateWeather}) {
+  let [city, setCity] = useState("");
+  let [error, seterror] = useState(false);
+  let data = (e) => {
+    setCity(e.target.value);
+    console.log(e.target.value);
+    seterror(false);
+  };
+
+
+  useEffect(() => {
+    city = "Delhi";
+    submit();
+  },[])
+  
+
+let submit = async (e) => {
+    try {
+      try {
+        e.preventDefault();
+      }
+      catch (e) {
+        console.log(e)
+      }
+      setCity("");
+      let data = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=a7f8a475b1903f10a61c5d0e00bbe743&units=metric`
+      );
+      let weatherData = await data.json();
+      let sunset = convertUnixTimestampTo12HourFormat(weatherData.sys.sunset);
+      let sunrise = convertUnixTimestampTo12HourFormat(weatherData.sys.sunrise);
+      let weather = {
+        city: city,
+        tempMax: weatherData.main.temp_max,
+        tempMin: weatherData.main.temp_min,
+        temp: weatherData.main.temp,
+        pressure: weatherData.main.pressure,
+        humidity: weatherData.main.humidity,
+        country: weatherData.sys.country,
+        sunrise:sunrise ,
+        sunset:sunset ,
+        feelsLike: weatherData.main.feels_like,
+        des: weatherData.weather[0].description,
+      };
+      console.log(weatherData);
+      updateWeather(weather);
+    } catch (e) {
+seterror(true)    }
+  };
+
+  return (
+    <div className="searchbox">
+      <form action="#">
+        <input
+          type="text"
+          placeholder="Location"
+          className="text"
+          onChange={data}
+          value={city}
+        />
+        <Button onClick={submit} className="s" type="submit">
+          <SearchIcon
+            className="se"
+            type="submit"
+            onclick={submit}
+          ></SearchIcon>
+        </Button>
+        {error && <p className="error">No such place exist ! </p>}
+      </form>
+    </div>
+  );
+}
